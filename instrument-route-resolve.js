2f84f9d5ce9005c9fc64e1f70171d509e308ccd6
Michael Leibman <michael.leibman@gmail.com>
2017-11-15 08:55:20 -0800

    Create instrument-route-resolve.js

---
+++ instrument-route-resolve.js
@@ -0,0 +1,33 @@
+/**
+ * Instrument route resolvers to time them and visualize on the performance timeline in Chrome DevTools.
+ */
+
+angular.element(document).injector().invoke(($injector, $rootScope) => {
+  $rootScope.$on('$routeChangeStart', (e, next, current) => {
+    if (!next.resolve) return;
+    const patchedSymbol = Symbol('patched');
+    
+    Object.keys(next.resolve).forEach(key => {
+      const resolver = next.resolve[key];
+      if (resolver[patchedSymbol]) return;
+      
+      next.resolve[key] = () => {
+        const name = `resolve: ${key}`;
+        console.time(name);
+        
+        const result = angular.isString(resolver) ?
+          $injector.get(resolver) :
+          $injector.invoke(resolver, null, null, key);
+          
+          if (result && result.then) {
+            result.then(() => console.timeEnd(name));
+          } else {
+            console.timeEnd(name)
+          }
+          
+        return result;
+      };
+      next.resolve[key][patchedSymbol] = true;
+    });
+  });
+});
